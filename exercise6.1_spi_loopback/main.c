a221d8901e57fa652735089561d17280b2da26c7
mbains <manjinder.bains@gmail.com>
2013-08-09 01:22:07 -0400

    initial SPI

--- exercise6.1_spi_loopback/main.c
+++ exercise6.1_spi_loopback/main.c
@@ -1,11 +1,49 @@
 #include <stm32f10x.h>
+#include "spi.h"
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_gpio.h>
 
+static uint8_t txbuf[4], rxbuf[4];
+static uint16_t txbuf16[4], rxbuf16[4];
+
 void Delay(uint32_t nTime);
 
+//initialize chip select
+void csInit(void)
+{
+    
+}
 
 int main(void){
+    int i,j;
+    
+    csInit();
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j< 4; j++)
+            txbuf[j] = i*4 + j;
+        
+        GPIO_WriteBit(GPIOC, GPIO_Pin_3, 0);
+        spiReadWrite(SPI2, rxbuf, txbuf, 4, SPI_SLOW);
+        GPIO_WriteBit(GPIOC, GPIO_Pin_3, 1);
+        
+        for(j = 0; j < 4; j++)
+            if(rxbuf[j]  != txbuf[j])
+                assert_failed(__FILE__, __LINE__);
+            
+    }
+    
+    for(i = 0; i < 8; i++){
+        for(j = 0; j < 4; j++) 
+            txbuf16[j] = i*4 + j + (i<<8);
+        GPIO_WriteBit(GPIOC, GPIO_Pin_3, 0);
+        spiReadWrite16(SPI2, rxbuf16, txbuf16, 4, SPI_SLOW);
+        GPIO_WriteBit(GPIOC, GPIO_Pin_3, 1);
+        for(j = 0; j < 4; j++)
+            if(rxbuf16[j]  != txbuf16[j])
+                assert_failed(__FILE__, __LINE__);
+    }
+    
     return 0;
 }
 
