d16956f04f0f819c45d7030dedf0b950ec5265dc
mbains <manjinder.bains@gmail.com>
2015-11-15 22:32:16 -0800

    wip hx711

--- hx711/hx711.c
+++ hx711/hx711.c
@@ -1,10 +1,76 @@
 #include "hx711.h"
 #include "stdint.h"
-static uint8_t _sck;
-static uint8_t _dout;
-static uint8_t _gain;
-static uint32_t _offset;
-static uint32_t _scale;
 
+typedef struct {
+    EZGPIO_Interface * _sck;
+    EZGPIO_Interface * _dout;
+    int8_t _gain;
+    uint32_t _offset;
+    uint32_t _scale;
+} hx_iface_t;
 
+static hx_iface_t m_hx_iface;
+
+int8_t hx711_init(EZGPIO_Interface * sck, EZGPIO_Interface *dout) {
+    m_hx_iface._sck = sck;
+    m_hx_iface._dout = dout;
+    m_hx_iface._gain = 1;
+  
+}
     
+int8_t hx711_is_ready() {
+    return EZGPIO_ReadInput(m_hx_iface._dout) == 0;
+}
+
+int32_t hx711_read() {
+    while(!hx711_is_ready());
+    
+    uint8_t data[3];
+    uint8_t j;
+    uint8_t i;
+    
+    for(j = 3; j--;) {
+        for(i = 8; i--;) {
+            EZGPIO_SetOutput(m_hx_iface._sck, 1);
+            data |= (EZGPIO_ReadInput(m_hx_iface._dout) << i);
+            EZGPIO_SetOutput(m_hx_iface._sck, 0);
+        }
+    }
+    
+    for (i = 0; i < m_hx_iface._gain; i++) {
+        EZGPIO_SetOutput(m_hx_iface._sck, 1);
+        EZGPIO_SetOutput(m_hx_iface._sck, 0);
+        
+    }
+    data[2] ^= 0x80;
+    
+    return ((uint32_t) data[2] << 16) | ((uint32_t) data[1] << 8) | (uint32_t) data[0];
+    
+}
+
+int32_t hx711_read_average(uint8_t times) {
+    int32_t sum = 0;
+    uint8_t i = 0;
+    for(; i < times; i++) {
+        sum += hx711_read();
+    }
+    return sum/times;
+}
+
+double hx711_get_value(uint8_t times) {
+    hx711_read_average(times) - m_hx_iface._offset;
+}
+
+float hx711_get_units(uint8_t times) {
+    return hx711_get_value(times) / m_hx_iface._scale;
+}
+
+void hx711_tare(uint8_t times) {
+    double sum = hx711_read_average(times);
+    hx711_set_offset(sum);
+}
+
+void hx711_set_offset(int32_t offset) {
+    m_hx_iface._offset = offset;
+}
+
--- hx711/hx711.h
+++ hx711/hx711.h
@@ -1,21 +1,25 @@
 #include <stdint.h>
+#include <gpio/gpio.h>
 
-int8_t hx711_init(uint8_t dout, uint8_t pd_sck, uint8_t gain);
 
-int8_t is_ready();
+int8_t hx711_init(EZGPIO_Interface * sck, EZGPIO_Interface *dout);
 
-void set_gain(uint8_t gain);
+int8_t hx711_is_ready();
 
-int32_t read();
+void hx711_set_gain(uint8_t gain);
 
-int32_t read_average(uint8_t times);
+int32_t hx711_read();
 
-double get_value(uint8_t times);
+int32_t hx711_read_average(uint8_t times);
 
-float get_units(uint8_t times);
+double hx711_get_value(uint8_t times);
 
-void tare(uint8_t times);
+float hx711_get_units(uint8_t times);
 
-void set_scale(float scale);
+void hx711_tare(uint8_t times);
+
+void hx711_set_scale(float scale);
+
+void hx711_set_offset(int32_t offset);
 
 
